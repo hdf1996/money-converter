ea1ed7d34f446ff4bbb6fe5548ab185459d4b615
Hugo Farji <hugo.farji@wolox.com.ar>
2017-12-30 20:36:19 -0300

    Basic compilation

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Convertidor de monedas!'
+    })
+  ],
+};
+
+module.exports = config;
