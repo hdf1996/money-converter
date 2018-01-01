d82e31458a52bfd0e000f3a6f0ccada8aef0c7f4
Hugo Farji <hugo.farji@wolox.com.ar>
2018-01-01 16:37:40 -0300

    Added web worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import { render } from 'react-dom'
 
 import App from './components/App'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 const app = document.createElement('div');
 document.body.appendChild(app);
 
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -2,12 +2,17 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.config.common.js');
 const webpack = require('webpack');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const config = merge(common, {
   plugins: [
     new UglifyJSPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest'
+    }),
+    new WorkboxPlugin({
+      clientsClaim: true,
+      skipWaiting: true
     })
   ]
 });
