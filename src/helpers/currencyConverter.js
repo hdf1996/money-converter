3924a86208fd6b6d51ac6ced2bdc858ce6a3b84e
Hugo David Farji <hugo.farji+github@gmail.com>
2019-09-09 09:35:43 -0300

    Update currencyConverter.js

--- src/helpers/currencyConverter.js
+++ src/helpers/currencyConverter.js
@@ -4,7 +4,9 @@ import { read, write } from './localStorage'
 
 const API_KEY = '8e3466abb4a8df6eba51518902394947'
 
-const API_URL = `https://free.currencyconverterapi.com/api/v5/convert?compact=y`
+const API_URL = `https://free.currencyconverterapi.com/api/v5/convert?compact=y&apiKey=6043e1c133665c1b8539
+
+`
 const convertCurrency = (originalCurrency, originalAmount, finalCurrency) => {
   const convertionName = `${originalCurrency}_${finalCurrency}`
   if(read(convertionName) !== null) return cachedFetch(convertionName)
